b13be42a4a0078820b91f5e202df4d79cba8ecf4
rvelasco <rogervz.1710@gmail.com>
2026-06-04 17:29:25 -0500

    feat(BD): Limpia de "migrator"

--- src/main/resources/db/migration/V7__retirar_reporte_productos_bajo_stock.sql
+++
@@ -1,9 +0,0 @@
-DELETE po
-FROM perfil_opcion po
-INNER JOIN opcion o ON o.id_opcion = po.id_opcion
-WHERE o.opcion_ruta = '/reportes/bajo-stock'
-   OR o.opcion_nombre = 'Productos Bajo Stock';
-
-DELETE FROM opcion
-WHERE opcion_ruta = '/reportes/bajo-stock'
-   OR opcion_nombre = 'Productos Bajo Stock';
